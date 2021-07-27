e2b8337ce790c86e1c11d9ad4829c1ab27b81ad9
thisisyoungjun <122899@naver.com>
2021-07-27 13:31:03 +0900

    커밋

---
+++ ArrayTest.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+
+        
+
+        String[] name={"A", "B","C"};
+        double[] score =new double[3];
+        int sum = 0;
+        double average =0.0;
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i]+"학생의 점수를 입력하세요");
+            score[i]=in.nextDouble();
+            sum+=score[i];
+        }
+        average=(double) sum/score.length;
+        System.out.println("총합 : "+ sum +"평균 : "+ average);
+    
+    }
+}
