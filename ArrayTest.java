64d2d49550823a62760bf43305af1775d4076683
thisisyoungjun <122899@naver.com>
2021-07-27 17:59:42 +0900

    커밋

--- ArrayTest.java
+++ ArrayTest.java
@@ -11,13 +11,18 @@ public class ArrayTest {
         double[] score =new double[3];
         int sum = 0;
         double average =0.0;
+        int max = 0;
         for (int i = 0; i < name.length; i++) {
             System.out.println(name[i]+"학생의 점수를 입력하세요");
             score[i]=in.nextDouble();
             sum+=score[i];
+            if (score[i]>max) {
+                max=(int) score[i];
+            }
         }
         average=(double) sum/score.length;
         System.out.println("총합 : "+ sum +"평균 : "+ average);
+        System.out.println("가장 큰 수는" + max);
     
     }
 }
