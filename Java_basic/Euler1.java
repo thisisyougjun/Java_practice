2e940ac39b366910238b21c17ddc6a54675ac31c
thisisyoungjun <122899@naver.com>
2021-08-21 15:27:53 +0900

    commit

--- Java_basic/Euler1.java
+++ Java_basic/Euler1.java
@@ -1,13 +1,14 @@
-public class Euler1 {
+public class Euler1{
     int max;
     public Euler1(int max){
         this.max=max;
     }
+
     public int sumOf3And5(){
         int sum =0;
         for (int i = 0; i < max; i++) {
             if (i%3==0 || i%5==0) {
-                sum +=1;
+                sum+=i;
             }
         }
         return sum;
@@ -18,4 +19,3 @@ public class Euler1 {
         System.out.println(new Euler1(1000).sumOf3And5());
     }
 }
-
--- Java_basic/algorithm/a6502.java
+++
@@ -1,7 +0,0 @@
-package algorithm;
-
-public class a6502 {
-    public static void main(String[] args) {
-        
-    }
-}
